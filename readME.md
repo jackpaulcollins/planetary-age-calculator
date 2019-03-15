ffeea86ff29608bc938a64db455a78edbe943ca9
Jack Collins <jackcollins@Jacks-MacBook-Pro.local>
2019-03-15 16:53:22 -0700

    finish readme

--- readME.md
+++ readME.md
@@ -1,3 +1,12 @@
+# Planetary Age Calculator
+ _This app calculates a user's age & life expectancy based on what planet they're on_
+
+### How to Use
+* _Clone this repository to your machine, and run 'npm install' to download the dependencies._
+* _Run 'npm run start' to build the project on a local server, or run 'npm test' to run tests on the project through Karma / Jasmine._
+
+
+
 ## Planning
 
 1. Configuration/dependencies
@@ -35,3 +44,12 @@
   * Spec 1: Determines in a user's age using their birthday, input: Dec. 8, 1988, output: "Age: 30"
   * Spec 2: Determines a user's age relevant to several planets (Mercury, Venus, Mars, Jupiter), input: age = 30, output: "Your age on Mercury is 7.2 years old".
   * Spec 3: Determines how many years a user would have left to live on each planet (using average life expectancy), input: age = 30, output: "On Jupiter, you're 355.8 years old! You should have died 277 years ago!"
+
+### Known Bugs
+
+*_The last test isn't worked out, since I changed the logic after initally writing the test_*
+
+#### License
+  * _This software is licensed under the MIT license_
+
+Copyright (c) 2019 **Jack Collins**
--- spec/spec.js
+++ spec/spec.js
@@ -1,36 +1,24 @@
-import { Planet } from "./../src/backend.js"
-import { ageOnMercury } from "./../src/backend.js"
-import { ageOnVenus } from "./../src/backend.js"
-import { ageOnMars } from "./../src/backend.js"
-import { ageOnJupiter } from "./../src/backend.js"
+
 import { lifeExpectancy } from "./../src/backend.js"
-import { yearsLeft } from "./../src/backend.js"
+import { ageOnPlanet } from "./../src/backend.js"
+
 
 describe('Ages', function() {
-  it('determine user ages for Mercury', function() {
-    let age = ageOnMercury(30)
-    expect(age).toEqual(7.199999999999999)
-  });
-
-  it('determine user ages for Venus', function() {
-    let age = ageOnVenus(30)
-    expect(age).toEqual(50)
-  });
-
-  it('determine user ages for Mars', function() {
-    let age = ageOnMars(30)
-    expect(age).toEqual(50)
-  });
-
-  it('determine user ages for Mercury', function() {
-    let age = ageOnJupiter(30)
-    expect(age).toEqual(50)
-  });
-});
+  it('determine user ages on planets', function() {
+    let ageOnJupiter = ageOnPlanet(30,"jupiter")
+    expect(ageOnJupiter).toEqual(355.8)
+    let ageOnMars = ageOnPlanet(30,"mars")
+    expect(ageOnMars).toEqual(56.4)
+    let ageOnVenus = ageOnPlanet(30,"venus")
+    expect(ageOnVenus).toEqual(18.6)
+    let ageOnMercury = ageOnPlanet(30,"mercury")
+    expect(ageOnMercury).toEqual(7.2)
+  })
+})
 
 describe('Life Expectancy', function(){
   it('determines the users life expectancy based on a given planet', function(){
     let lifeExp = lifeExpectancy(30,"jupiter")
-    expect(lifeExp).toEqual(10)
+    expect(lifeExp).toEqual(2.52)
   });
 });
