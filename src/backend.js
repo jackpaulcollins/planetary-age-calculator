989ed57a9a984120c51160c595bc38924c54d95d
Jack Collins <jackcollins@Jacks-MacBook-Pro.local>
2019-03-15 14:54:35 -0700

    change result of math functions to only return two decimal points

--- src/backend.js
+++ src/backend.js
@@ -10,10 +10,10 @@ const mars = new Planet("Mars",1.88);
 const jupiter = new Planet("Jupiter",11.86);
 
 export function ageOnPlanet(age, planet){
-  if (planet == "mercury"){return age * .24}
-  else if (planet == venus){return age * .62}
-  else if (planet == mars){return age * 1.88}
-  else {return age * 11.86}
+  if (planet == "mercury"){return (age * .24).toFixed(2)}
+  else if (planet == "venus"){return (age * .62).toFixed(2)}
+  else if (planet == "mars"){return (age * 1.88).toFixed(2)}
+  else {return (age * 11.86).toFixed(2)}
 }
 
 
@@ -28,13 +28,13 @@ export function lifeExpectancy(age,planet){
     return `You still have ${usLifeExp - planetAge} years to live!`
     } else {
     return `You should have died ${planetAge - usLifeExp} years ago!`}
-    } else if (planet == venus){
+  } else if (planet == "venus"){
     let planetAge = ageOnPlanet(age,venus)
     if (planetAge < yearsStillLeft){
     return `You still have ${usLifeExp - planetAge} years to live!`
     } else {
     return `You should have died ${planetAge - usLifeExp} years ago!`}
-  } else if (planet == mars){
+  } else if (planet == "mars"){
     let planetAge = ageOnPlanet(age,mars)
     if (planetAge < yearsStillLeft){
     return `You still have ${usLifeExp - planetAge} years to live!`
